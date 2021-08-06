c8234516be000c302967fadd3484c7f984fc8cf9
Bradley Corner <bc80921@gmail.com>
2021-08-06 01:14:49 -0400

    additional documentation

--- src/deployment/variables.tf
+++ src/deployment/variables.tf
@@ -5,6 +5,9 @@ variable "project_id" {
 variable "certmanager_email" {
   description = "Email used to retrieve SSL certificates from Let's Encrypt"
 }
+# This variable turns off or on the protection from deletion feature. The
+# default action of true means that this resource cannot be deleted without
+# first changing this value to false and re-runing the terraform apply.
 variable "deletion_protection" {
   type = string
   description = "Protect instance from deletion."
